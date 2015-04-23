0b20a1b3e54370945e983726a0a9d3e6318d3d87
hangsu <suhangpro@gmail.com>
2015-04-23 17:29:28 -0400

    parallelism in retrieval code

--- retrieve_shapes_cnn.m
+++ retrieve_shapes_cnn.m
@@ -22,6 +22,8 @@ function [ results,info ] = retrieve_shapes_cnn( shape, feat, varargin )
 %       preloaded cnn model (required when feat.modelName is not available)
 %   `gpuMode`:: false
 %       set to true to compute on GPU
+%   `numWorkers`:: 1
+%       number of workers used to compute pairwise 
 %   `nTop`:: Inf
 %       number of images in results
 %   `querySets`:: {'test'}
@@ -37,11 +39,20 @@ function [ results,info ] = retrieve_shapes_cnn( shape, feat, varargin )
 opts.method = 'avgdesc';
 opts.net = [];
 opts.gpuMode = false;
+opts.numWorkers = 1;
 opts.nTop = Inf;
 opts.querySets = {'test'};
 opts.refSets = {'test'};
 opts.metric = 'L2';
 opts = vl_argparse(opts,varargin);
+
+if opts.numWorkers>1, 
+    pool = gcp('nocreate');
+    if isempty(pool) || pool.NumWorkers<opts.numWorkers, 
+        if ~isempty(pool), delete(pool); end
+        pool = parpool(opts.numWorkers);
+    end
+end
 
 % -------------------------------------------------------------------------
 %                       sort imdb.images & feat.x w.r.t (sid,view) or (id)
@@ -183,17 +194,20 @@ end
 % -------------------------------------------------------------------------
 switch opts.method,
     case 'mindist',
-        dists0 = vl_alldist2(queryX',refX',opts.metric);
+        dists0 = par_alldist(queryX',refX',...
+            'measure',opts.metric,'numWorkers',opts.numWorkers);
         dists = -1*vl_nnpool(-1*single(dists0),[nDescPerShape nRefDescPerShape], ...
             'stride', [nDescPerShape nRefDescPerShape], ...
             'method', 'max');
     case 'avgdist',
-        dists0 = vl_alldist2(queryX',refX',opts.metric);
+        dists0 = par_alldist(queryX',refX',...
+            'measure',opts.metric,'numWorkers',opts.numWorkers);
         dists = vl_nnpool(single(dists0),[nDescPerShape nRefDescPerShape], ...
             'stride', [nDescPerShape nRefDescPerShape], ...
             'method', 'avg');
     case 'avgmindist',
-        dists0 = vl_alldist2(queryX',refX',opts.metric);
+        dists0 = par_alldist(queryX',refX',...
+            'measure',opts.metric,'numWorkers',opts.numWorkers);
         dists_1 = -1*vl_nnpool(-1*single(dists0),[nDescPerShape 1], ...
             'stride',[nDescPerShape 1], 'method','max');
         dists_1 = vl_nnpool(dists_1,[1 nRefDescPerShape], ...
@@ -204,7 +218,8 @@ switch opts.method,
             'stride',[nDescPerShape 1], 'method','avg');
         dists = 0.5*(dists_1+dists_2);
     case 'minavgdist',
-        dists0 = vl_alldist2(queryX',refX',opts.metric);
+        dists0 = par_alldist(queryX',refX',...
+            'measure',opts.metric,'numWorkers',opts.numWorkers);
         dists_1 = vl_nnpool(single(dists0),[nDescPerShape 1], ...
             'stride',[nDescPerShape 1], 'method','avg');
         dists_1 = -1*vl_nnpool(-1*dists_1,[1 nRefDescPerShape], ...
@@ -219,14 +234,16 @@ switch opts.method,
             [nDescPerShape nQueryShapes*nDims]),1),[nQueryShapes nDims]);
         descs = reshape(mean(reshape(refX, ...
             [nRefDescPerShape nRefShapes*nDims]),1),[nRefShapes nDims]);
-        dists0 = vl_alldist2(desc',descs',opts.metric);
+        dists0 = par_alldist(desc',descs',...
+            'measure',opts.metric,'numWorkers',opts.numWorkers);
         dists = dists0;
     case 'maxdesc',
         desc = reshape(max(reshape(queryX, ...
             [nDescPerShape nQueryShapes*nDims]),[],1),[nQueryShapes nDims]);
         descs = reshape(max(reshape(refX, ...
             [nRefDescPerShape nRefShapes*nDims]),[],1),[nRefShapes nDims]);
-        dists0 = vl_alldist2(desc',descs',opts.metric);
+        dists0 = par_alldist(desc',descs',...
+            'measure',opts.metric,'numWorkers',opts.numWorkers);
         dists = dists0;
     otherwise,
         error('Unknown retrieval method: %s', opts.method);
@@ -240,7 +257,7 @@ if ~isempty(shape), % retrieval given a query shape
     results = refShapeIds(I(1:min(opts.nTop,nRefShapes)));
     info = [];
 else                % no query given, evaluation within dataset
-    % pr curves 
+    % pr curves
     recall = zeros(nQueryShapes, nRefShapes+1);
     precision = zeros(nQueryShapes, nRefShapes+1);
     ap = zeros(nQueryShapes, 1);
@@ -251,26 +268,50 @@ else                % no query given, evaluation within dataset
     ap_i = zeros(nQueryShapes, 1);
     auc_i = zeros(nQueryShapes, 1);
     
-    for q = 1:nQueryShapes, 
-        [r, p, info] = vl_pr(...
-            (shapeGtClasses(refShapeIds)==shapeGtClasses(queryShapeIds(q)))-0.5, ... % LABELS
-            -1*dists(q,:), ... % SCORES
-            'Interpolate', false); 
-        recall(q,:) = r;
-        precision(q,:) = p;
-        ap(q) = info.ap;
-        auc(q) = info.auc;
-        % interpolated
-        [r, p, info] = vl_pr(...
-            (shapeGtClasses(refShapeIds)==shapeGtClasses(queryShapeIds(q)))-0.5, ... % LABELS
-            -1*dists(q,:), ... % SCORES
-            'Interpolate', true); 
-        recall_i(q,:) = r;
-        precision_i(q,:) = p;
-        ap_i(q) = info.ap;
-        auc_i(q) = info.auc;
+    refClass = shapeGtClasses(refShapeIds);
+    queryClass = shapeGtClasses(queryShapeIds);
+    if opts.numWorkers>1,
+        parfor q = 1:nQueryShapes,
+            [r, p, info] = vl_pr(...
+                (refClass==queryClass(q))-0.5, ... % LABELS
+                -1*dists(q,:), ... % SCORES
+                'Interpolate', false);
+            recall(q,:) = r;
+            precision(q,:) = p;
+            ap(q) = info.ap;
+            auc(q) = info.auc;
+            % interpolated
+            [r, p, info] = vl_pr(...
+                (refClass==queryClass(q))-0.5, ... % LABELS
+                -1*dists(q,:), ... % SCORES
+                'Interpolate', true);
+            recall_i(q,:) = r;
+            precision_i(q,:) = p;
+            ap_i(q) = info.ap;
+            auc_i(q) = info.auc;
+        end
+    else
+        for q = 1:nQueryShapes,
+            [r, p, info] = vl_pr(...
+                (refClass==queryClass(q))-0.5, ... % LABELS
+                -1*dists(q,:), ... % SCORES
+                'Interpolate', false);
+            recall(q,:) = r;
+            precision(q,:) = p;
+            ap(q) = info.ap;
+            auc(q) = info.auc;
+            % interpolated
+            [r, p, info] = vl_pr(...
+                (refClass==queryClass(q))-0.5, ... % LABELS
+                -1*dists(q,:), ... % SCORES
+                'Interpolate', true);
+            recall_i(q,:) = r;
+            precision_i(q,:) = p;
+            ap_i(q) = info.ap;
+            auc_i(q) = info.auc;
+        end
     end
-    clear info;
+    info = [];
     info.recall = recall;
     info.precision = precision;
     info.ap = ap;
