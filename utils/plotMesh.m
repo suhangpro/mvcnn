9fd4dd957ca41fa182d307cab962a7d7e31261b1
Badger <kalo@cs.umass.edu>
2015-08-02 18:52:59 -0400

    minor changes for loading and plotting meshes, major changes to render_views, a new script to compute a descriptor for a shape or shapes on the fly

--- utils/plotMesh.m
+++ utils/plotMesh.m
@@ -1,122 +1,27 @@
-function plotMesh(mesh, style, az, el)
-
-% mesh.F = mesh.f;
-% mesh.V = mesh.v;
-% plotMesh(mesh)
-% plotMesh(mesh, style)
-%
-% style     {figure} | mesh | solid | solidbw
+function h = plotMesh(mesh, style, az, el)
 
 if nargin < 2
     style = 'figure';
 end
-    
-if strcmpi(style, 'figure')
-    
-    h = trimesh(mesh.F',mesh.V(1,:)',mesh.V(2,:)',mesh.V(3,:)', 'FaceColor', 'none', ...
-                'EdgeColor', 'k','EdgeLighting','flat','AmbientStrength',.4);
 
-    set(gcf, 'Renderer', 'OpenGL');
-
-    axis equal;
-    axis tight;
-    grid on;
-    if nargin >= 4 view(az,el); else view(3); end
-
-elseif strcmpi(style, 'mesh')
-    
-    h = trimesh(mesh.F',mesh.V(1,:)',mesh.V(2,:)',mesh.V(3,:)', 'FaceColor', 'none', 'EdgeColor', 'w', ...
-        'AmbientStrength', 0.4, 'EdgeLighting', 'flat');
-  
+if strcmpi(style, 'mesh')    
+    h = trimesh(mesh.F', mesh.V(1,:)', mesh.V(2,:)', mesh.V(3,:)', 'FaceColor', 'none', 'EdgeColor', 'w', ...
+        'AmbientStrength', 0.4, 'EdgeLighting', 'flat');    
     set(gcf, 'Color', 'k', 'Renderer', 'OpenGL');
-    set(gca, 'Projection', 'perspective');
-
+    set(gca, 'Projection', 'perspective');    
     axis equal;
     axis off;
-    if nargin >= 4 view(az,el); else view(3); end
-    % camlight;
-
+    view(az,el);    
 elseif strcmpi(style, 'solid')
-    h = trimesh(mesh.F',mesh.V(1,:)',mesh.V(2,:)',mesh.V(3,:)', 'FaceColor', 'w', 'EdgeColor', 'none', ...
-        'AmbientStrength', 0.2, 'FaceLighting', 'phong', 'SpecularStrength', 1.0, 'SpecularExponent', 100);
-    
+    h = trimesh(mesh.F', mesh.V(1,:)', mesh.V(2,:)' ,mesh.V(3,:)', 'FaceColor', 'w', 'EdgeColor', 'none', ...
+        'AmbientStrength', 0.2, 'FaceLighting', 'phong', 'SpecularStrength', 0.0, 'SpecularExponent', 100);    
     if isfield(mesh, 'Nv')
-        set(h, 'VertexNormals', -mesh.Nv(1:3,:)'); 
-    end
-    
-    if isfield(mesh, 'C')
-        set(h, 'FaceVertexCData', mesh.C);
-        set(h, 'FaceColor', 'flat');
-    end
-    
-    set(gcf, 'Color', 'w', 'Renderer', 'OpenGL');
-    set(gca, 'Projection', 'perspective');
-    
-    axis equal;    
+        set(h, 'VertexNormals', -mesh.Nv(1:3,:)');
+    end    
+    set(gcf, 'Color', 'w');
+    set(gca, 'Projection', 'perspective');    
+    axis equal;
     axis off;
-    if nargin >= 4 view(az,el); else view(3); end
-    camlight;
-    
-elseif strcmpi(style, 'solidbw')
-    
-    h = trimesh(mesh.F',mesh.V(1,:)',mesh.V(2,:)',mesh.V(3,:)', 'FaceColor', 'w', 'EdgeColor', 'k', ...
-        'AmbientStrength', 0.7, 'FaceLighting', 'flat', 'EdgeLighting', 'none');
-        
-    set(gcf, 'Color', 'w', 'Renderer', 'OpenGL');
-    set(gca, 'Projection', 'perspective');
-
-    axis equal;    
-    axis off;
-    if nargin >= 4 view(az,el); else view(3); end
-    % camlight;
-
-elseif strcmpi(style, 'solidbws')
-    
-    h = trimesh(mesh.F',mesh.V(1,:)',mesh.V(2,:)',mesh.V(3,:)', 'FaceColor', 'w', 'EdgeColor', 'k', ...
-        'AmbientStrength', 0.7, 'FaceLighting', 'phong', 'EdgeLighting', 'none');
-        
-    set(gcf, 'Color', 'w', 'Renderer', 'OpenGL');
-    set(gca, 'Projection', 'perspective');
-
-    axis equal;    
-    axis off;
-    if nargin >= 4 view(az,el); else view(3); end
-    % camlight;
-    
-elseif strcmpi(style, 'ghost')
-    
-    h = trimesh(mesh.F',mesh.V(1,:)',mesh.V(2,:)',mesh.V(3,:)', 'FaceColor', 'w', 'EdgeColor', 'k', ...
-        'AmbientStrength', 0.4, 'FaceLighting', 'phong', 'EdgeLighting', 'none', 'FaceAlpha',.5);
-        
-    set(gcf, 'Color', 'w', 'Renderer', 'OpenGL');
-    set(gca, 'Projection', 'perspective');
-
-    axis equal;    
-    axis off;
-    if nargin >= 4 view(az,el); else view(3); end
-    % camlight;
-    
-elseif strcmpi(style, 'ghosts')
-    h = trimesh(mesh.F',mesh.V(1,:)',mesh.V(2,:)',mesh.V(3,:)', 'FaceColor', 'w', 'EdgeColor', 'none', ...
-        'AmbientStrength', 0.2, 'FaceLighting', 'phong', 'SpecularStrength', 1.0, 'SpecularExponent', 100, 'FaceAlpha',.5);
-    
-    if isfield(mesh, 'Nv')
-        set(h, 'VertexNormals', -mesh.Nv(1:3,:)'); 
-    end
-    
-    if isfield(mesh, 'C')
-        set(h, 'FaceVertexCData', mesh.C);
-        set(h, 'FaceColor', 'interp');
-    end
-    
-    set(gcf, 'Color', 'w', 'Renderer', 'OpenGL');
-    set(gca, 'Projection', 'perspective');
-    
-    axis equal;    
-    axis off;
-    if nargin >= 4 view(az,el); else view(3); end
-    camlight;
+    view(az,el);
+    camlight;    
 end
-
-
-
--- utils/render_views.m
+++ utils/render_views.m
@@ -1,68 +1,123 @@
 function ims = render_views( mesh, varargin )
-%RENDER_VIEWS render a 3d shape from multiple views 
+%RENDER_VIEWS render a 3d shape from multiple views
 %   mesh::
 %       a mesh object containing fileds
 %           .F 3 x #faces (1-based indexing)
 %           .V 3 x #vertices
 %       OR a path to .off file
-%   `az`:: [0:30:330]
-%       horizontal viewing angles
-%   `el`:: 30
-%       vertical elevation
-%   `colorMode`:: 'gray'
-%       color mode of output images (only 'gray' is supported now)
-%   `outputSize`:: 224
+%   `az`:: (default) [0:30:330]
+%       horizontal viewing angles, use this setting for shapes that are
+%       upright oriented according to +Z axis!
+%   `el`:: (default) 30
+%       vertical elevation, , use this setting for shapes that are
+%       upright oriented according to +Z axis!
+%   `use_dodecahedron_views`:: (default) false
+%       ignores az, el -  places cameras on the vertices of a unit
+%       dodecahedron, rotates them, and produces 160 views.
+%       use this setting for shapes that are not upright oriented.
+%   `colorMode`:: (default)  'rgb'
+%       color mode of output images ('rgb' or 'gray')
+%   `outputSize`::  (default)  224
 %       output image size (both dimensions)
-%   `minMargin`:: 0.1
+%   `minMargin`:: (default)  0.1
 %       minimun margin ratio in output images
-%   `maxArea`:: 0.3
-%       maximun area ratio in output images 
-%   `figHandle`:: []
-%       handle to temporary figure
-%   
+%   `maxArea`:: (default)  0.3
+%       maximun area ratio in output images
+%   `figHandle`:: (default) []
+%       handle to existing figure
 
 opts.az = [0:30:330];
 opts.el = 30;
-opts.colorMode = 'gray';
+opts.use_dodecahedron_views = false;
+opts.colorMode = 'rgb';
 opts.outputSize = 224;
 opts.minMargin = 0.1;
 opts.maxArea = 0.3;
 opts.figHandle = [];
 opts = vl_argparse(opts,varargin);
 
-if ~strcmpi(opts.colorMode,'gray'), 
-    error('color mode (%s) not supported.',opts.colorMode);
+if isempty(opts.figHandle)
+    opts.figHandle = figure;
 end
 
-if isempty(opts.figHandle), 
-    fh = figure;
-else
-    fh = opts.figHandle;
+if opts.use_dodecahedron_views
+    phi = (1+sqrt(5))/2;
+    
+    vertices = [
+        1, 1, 1;
+        1, 1, -1;
+        1, -1, 1;
+        1, -1, -1;
+        -1, 1, 1;
+        -1, 1, -1;
+        -1, -1, 1;
+        -1, -1, -1;
+        
+        0, 1/phi, phi;
+        0, 1/phi, -phi;
+        0, -1/phi, phi;
+        0, -1/phi, -phi;
+        
+        phi, 0, 1/phi;
+        phi, 0, -1/phi;
+        -phi, 0, 1/phi;
+        -phi, 0, -1/phi;
+        
+        1/phi, phi, 0;
+        -1/phi, phi, 0;
+        1/phi, -phi, 0;
+        -1/phi, -phi, 0;
+        ];
+    
+    num_vertices = size(vertices,1);
+    
+    opts.az = zeros(num_vertices, 1);
+    opts.el = zeros(num_vertices, 1);
+    
+    for i=1:num_vertices
+        opts.az(i) = (vertices(i,2) / vertices(i,1));
+        opts.az(i) = atan(opts.az(i)) / pi * 180 + 180 * (sign(vertices(i,1))==-1);
+        
+        opts.el(i) = (vertices(i,3) / sqrt(vertices(i,1) * vertices(i,1) + vertices(i,2) * vertices(i,2)));
+        opts.el(i) = atan(opts.el(i)) / pi * 180;
+    end
 end
 
-if ischar(mesh), 
-    if strcmpi(mesh(end-2:end),'off'), 
+if ischar(mesh),
+    if strcmpi(mesh(end-2:end),'off') || strcmpi(mesh(end-2:end),'obj')
         mesh = loadMesh(mesh);
     else
         error('file type (.%s) not supported.',mesh(end-2:end));
     end
 end
 
-fh_id = fh; if ~isnumeric(fh_id), fh_id = fh_id.Number; end
-tmp_file = sprintf('tmp_%08s.png',dec2hex(fh_id));
-ims = cell(1,length(opts.az));
-for i=1:length(opts.az), 
-    plotMesh(mesh,'solid',opts.az(i),opts.el);
-    print(fh,'-dpng',tmp_file);
-    im = imread(tmp_file);
-    if strcmpi(opts.colorMode,'gray'), im = rgb2gray(im); end
-    ims{i} = resize_im(im,opts.outputSize,opts.minMargin,opts.maxArea);
+if opts.use_dodecahedron_views
+    ims = cell(1, length(opts.az) * 8);
+    im_counter = 0;
+    for i=1:length(opts.az)
+        plotMesh(mesh,'solid',opts.az(i),opts.el(i));
+        for cv=1:8
+            im_counter = im_counter + 1;
+            ims{im_counter} = frame2im(getframe(opts.figHandle));            
+            if strcmpi(opts.colorMode,'gray'), ims{im_counter} = rgb2gray(ims{im_counter}); end
+            ims{im_counter} = resize_im(ims{im_counter}, opts.outputSize, opts.minMargin, opts.maxArea);            
+            camroll(45);
+        end        
+    end
+else
+    ims = cell(1,length(opts.az));
+    for i=1:length(opts.az),
+        plotMesh(mesh,'solid',opts.az(i),opts.el);
+        ims{i} = frame2im(getframe(opts.figHandle));
+        if strcmpi(opts.colorMode,'gray'), ims{i} = rgb2gray(ims{i}); end
+        ims{i} = resize_im(ims{i}, opts.outputSize, opts.minMargin, opts.maxArea);
+    end
 end
 
-if isempty(opts.figHandle), close(fh); end
-delete(tmp_file);
-
 end
+
+
+
 
 function im = resize_im(im,outputSize,minMargin,maxArea)
 
@@ -73,7 +128,7 @@ nCh = size(im,3);
 mask = ~im2bw(im,1-1e-10);
 mask = imfill(mask,'holes');
 % blank image (all white) is outputed if not object is observed
-if isempty(find(mask, 1)), 
+if isempty(find(mask, 1)),
     im = uint8(255*ones(outputSize,outputSize,nCh));
     return;
 end
