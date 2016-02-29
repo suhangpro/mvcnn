279532a91c57f315de6ca9cce42e90398e9c2ff8
Hang Su <suhangpro@gmail.com>
2016-02-29 14:49:04 -0500

    more accurate time estimation for par_alldist

--- utils/par_alldist.m
+++ utils/par_alldist.m
@@ -67,6 +67,7 @@ x1cell = cell(1,t);
 x2cell = cell(1,t);
 distcell = cell(1,t);
 if opts.verbose, 
+    ts = tic;
     fprintf('[1/3] Partitioning into %dx ((%d,%d),(%d,%d)) parts \n', ...
         t, D,w,D,w);
 end
@@ -85,12 +86,16 @@ for i = 1:t,
         if mod(i,200)==0, fprintf(' [%d/%d]\n',i,t); end;
     end
 end
-if opts.verbose, fprintf(' done!\n'); end;
+if opts.verbose, fprintf(' done! (%s)\n', timestr(toc(ts))); end;
 
 % estimate speed
 tmp = rand(1000,1000);
 tt=tic;vl_alldist2(tmp,tmp,opts.measure);tc=toc(tt);
-estTime = (D*n1*n2/1e9)*tc;
+if withSelf, 
+    estTime = (D*(n1*n2/2-n1/2)/1e9)*tc;
+else
+    estTime = (D*n1*n2/1e9)*tc;
+end
 
 % real work
 %{-
@@ -101,17 +106,25 @@ if isempty(pool) || pool.NumWorkers<opts.numWorkers,
     pool = parpool(opts.numWorkers);
 end
 if opts.verbose, 
+    ts = tic;
     fprintf('[2/3] Computing distances using %d workers (~%s) ...', ...
     pool.NumWorkers, timestr(estTime/pool.NumWorkers));
 end
 %}
+fprintf('\n'); 
+parfor_progress(t); 
 parfor i=1:t,
     distcell{i} = vl_alldist2(x1cell{i},x2cell{i},opts.measure);
+    parfor_progress(); 
 end
-if opts.verbose, fprintf(' done!\n'); end;
+parfor_progress(0); 
+if opts.verbose, fprintf(' done! (%s)\n', timestr(toc(ts))); end;
 
 % assemble results
-if opts.verbose, fprintf('[3/3] Assembling distance matrix \n'); end;
+if opts.verbose, 
+    ts = tic;
+    fprintf('[3/3] Assembling distance matrix \n'); 
+end;
 Dist = zeros(n1,n2,typeX1);
 for i = 1:t,
     i1 = mod(i-1,npar(1))+1;
@@ -127,7 +140,7 @@ for i = 1:t,
         if mod(i,200)==0, fprintf(' [%d/%d]\n',i,t); end;
     end
 end
-if opts.verbose, fprintf(' done!\n'); end;
+if opts.verbose, fprintf(' done! (%s)\n', timestr(toc(ts))); end;
 
 end
 
